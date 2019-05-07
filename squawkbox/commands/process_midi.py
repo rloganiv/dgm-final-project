7d7e254ff071a42df85f88461f198273a5d48534
Robert Logan <rloganiv@gmail.com>
2019-05-07 11:41:35 -0700

    WORKING MIDI PARSER!!!

--- squawkbox/commands/process_midi.py
+++ squawkbox/commands/process_midi.py
@@ -100,9 +100,10 @@ class MidiTrack:
             raise MidiError('Cannot parse a MIDI track info without a header.')
         byte_queue = deque(chunk)
         events = []
+        event = None
         while len(byte_queue) > 0:
             delta_time = _parse_variable_length_quantity(byte_queue)
-            event = _parse_event(byte_queue)
+            event = _parse_event(byte_queue, event)
             logger.debug(f'Delta={delta_time}, Event={event}')
             events.append((delta_time, event))
         return cls(events)
@@ -122,16 +123,19 @@ def _parse_variable_length_quantity(byte_queue: Deque[int]) -> int:
     return quantity
 
 
-def _parse_event(byte_queue: Deque[int]) -> 'Event':
-    event_type = byte_queue.popleft()
-    if event_type in SysexEvent.EVENT_TYPES:
-        return SysexEvent.from_byte_queue(byte_queue, event_type)
-    elif event_type in MetaEvent.EVENT_TYPES:
-        return MetaEvent.from_byte_queue(byte_queue, event_type)
-    elif (event_type >> 4) in MidiEvent.EVENT_TYPES:
-        return MidiEvent.from_byte_queue(byte_queue, event_type)
+def _parse_event(byte_queue: Deque[int], prev_event: 'Event' = None) -> 'Event':
+    prefix = byte_queue.popleft()
+    if prefix in SysexEvent.PREFIXES:
+        return SysexEvent.from_byte_queue(byte_queue, prefix)
+    elif prefix in MetaEvent.PREFIXES:
+        return MetaEvent.from_byte_queue(byte_queue, prefix)
+    elif (prefix >> 4) in MidiEvent.PREFIXES:
+        return MidiEvent.from_byte_queue(byte_queue, prefix)
+    elif (prefix < 0x80) and isinstance(prev_event, MidiEvent):
+        byte_queue.appendleft(prefix)
+        return MidiEvent.from_byte_queue(byte_queue, prev_event.prefix)
     else:
-        raise MidiError(f'Encountered unknown event type "{event_type:02x}".')
+        raise MidiError(f'Encountered unknown event prefix "{prefix:02x}".')
 
 
 def _pop_bytes(byte_queue: Deque[int], n: int) -> bytes:
@@ -144,7 +148,7 @@ class Event:
         return f'{self.__class__.__name__}({args})'
 
     @classmethod
-    def from_byte_queue(cls, byte_queue: Deque[int], event_type: int) -> 'Event':
+    def from_byte_queue(cls, byte_queue: Deque[int], prefix: int) -> 'Event':
         raise NotImplementedError('Method must be overridden by subclasses.')
 
 
@@ -154,22 +158,18 @@ class SysexEvent(Event):
 
     Don't know what these do, or really care...
     """
-    EVENT_TYPES = {
-        0xF0: 'F0',
-        0xF7: 'F7'
-    }
-    def __init__(self, event_type: str, length: int, data: bytes) -> None:
-        self.event_type = event_type
+    PREFIXES = {0xF0, 0xF7}
+    def __init__(self, prefix: int, length: int, data: bytes) -> None:
+        self.prefix = prefix
         self.length = length
         self.data = data
 
     @classmethod
     @overrides
-    def from_byte_queue(cls, byte_queue: Deque[int], event_type: int) -> 'SysexEvent':
-        event_type_str = SysexEvent.EVENT_TYPES[event_type]
+    def from_byte_queue(cls, byte_queue: Deque[int], prefix: int) -> 'SysexEvent':
         length = _parse_variable_length_quantity(byte_queue)
         data = b''.join(bytes(byte_queue.popleft()) for _ in range(length))
-        return cls(event_type_str, length, data)
+        return cls(prefix, length, data)
 
 
 class MetaEvent(Event):
@@ -183,10 +183,8 @@ class MetaEvent(Event):
 
     NOTE: In addition to the parameters above, assorted message parameters may also be assigned.
     """
+    PREFIXES = {0xFF}
     EVENT_TYPES = {
-        0xFF: 'FF'
-    }
-    SUBEVENT_TYPES = {
         0x00: 'SequenceNumber',
         0x01: 'Text',
         0x02: 'Copyright',
@@ -203,31 +201,32 @@ class MetaEvent(Event):
         0x59: 'KeySignature'
     }
 
-    def __init__(self, event_type: str) -> None:
+    def __init__(self, prefix: int, event_type: str) -> None:
+        self.prefix = prefix
         self.event_type = event_type
 
     @classmethod
     @overrides
-    def from_byte_queue(cls, byte_queue: Deque[int], event_type) -> 'MetaEvent':
+    def from_byte_queue(cls, byte_queue: Deque[int], prefix: int) -> 'MetaEvent':
         # Construct basic event.
-        subevent_type = byte_queue.popleft()
-        if subevent_type not in MetaEvent.SUBEVENT_TYPES:
-            raise MidiError(f'Unknown meta event type "{subevent_type:02x}".')
-        subevent_type_str = MetaEvent.SUBEVENT_TYPES[subevent_type]
-        meta_event = cls(subevent_type_str)
+        event_byte = byte_queue.popleft()
+        if event_byte not in MetaEvent.EVENT_TYPES:
+            raise MidiError(f'Unknown meta event type "{event_byte:02x}".')
+        event_type = MetaEvent.EVENT_TYPES[event_byte]
+        meta_event = cls(prefix, event_type)
 
         # Text events.
-        if subevent_type_str in ['Text', 'Copyright', 'SequenceName', 'InstrumentName', 'Lyric', 'Marker', 'CuePoint']:
+        if event_type in ['Text', 'Copyright', 'SequenceName', 'InstrumentName', 'Lyric', 'Marker', 'CuePoint']:
             length = byte_queue.popleft()
             meta_event.text = _pop_bytes(byte_queue, length).decode('ascii')
 
         # Tempo
-        if subevent_type_str == 'SetTempo':
+        if event_type == 'SetTempo':
             byte_queue.popleft()
             meta_event.text = str(int.from_bytes(_pop_bytes(byte_queue, 3), byteorder='big'))
 
         # Time Signature
-        if subevent_type_str == 'TimeSignature':
+        if event_type == 'TimeSignature':
             byte_queue.popleft()
             numerator = byte_queue.popleft()
             denominator = 2**byte_queue.popleft()
@@ -235,20 +234,20 @@ class MetaEvent(Event):
             meta_event.data = _pop_bytes(byte_queue, 2)
 
         # Key Signature
-        if subevent_type_str == 'KeySignature':
+        if event_type == 'KeySignature':
             byte_queue.popleft()
             sf = int.from_bytes(byte_queue.popleft(), byteorder='big', signed=True)
             tonality = 'minor' if byte_queue.popleft() else 'major'
             meta_event.text = f'{tonality}{sf}'
 
         # Either store excess data in a byte string or discard.
-        if subevent_type_str == 'SequenceNumber':
+        if event_type == 'SequenceNumber':
             meta_event.data = _pop_bytes(byte_queue, 3)
-        if subevent_type_str == 'ChannelPrefix':
+        if event_type == 'ChannelPrefix':
             meta_event.data = _pop_bytes(byte_queue, 2)
-        if subevent_type_str == 'EndOfTrack':
+        if event_type == 'EndOfTrack':
             byte_queue.popleft()
-        if subevent_type_str == 'SmtpeOffset':
+        if event_type == 'SmtpeOffset':
             meta_event.data = _pop_bytes(byte_queue, 6)
 
         return meta_event
@@ -267,6 +266,7 @@ class MidiEvent(Event):
 
     NOTE: In addition to the parameters above, assorted message parameters may also be assigned.
     """
+    PREFIXES = {0x8,0x9, 0xA, 0xB, 0xC, 0xD, 0xE}
     EVENT_TYPES = {
         0x8: 'NoteOff',
         0x9: 'NoteOn',
@@ -276,25 +276,25 @@ class MidiEvent(Event):
         0xD: 'ChannelKeyPressure',
         0xE: 'PitchBend'
     }
-    def __init__(self, event_type: str, channel: int) -> None:
+    def __init__(self, prefix: int, event_type: str, channel: int) -> None:
+        self.prefix = prefix
         self.event_type = event_type
         self.channel = channel
 
     @classmethod
     @overrides
-    def from_byte_queue(cls, byte_queue: Deque[int], event_type: int) -> 'MidiEvent':
+    def from_byte_queue(cls, byte_queue: Deque[int], prefix: int) -> 'MidiEvent':
         # Construct basic event.
-        event_type_str = MidiEvent.EVENT_TYPES[event_type >> 4]
-        channel = event_type & 0x0F
-        midi_event = cls(event_type_str, channel)
+        event_type = MidiEvent.EVENT_TYPES[prefix >> 4]
+        channel = prefix & 0x0F
+        midi_event = cls(prefix, event_type, channel)
 
         # Carefully parse data for events we care about.
-        if event_type_str in ['NoteOff', 'NoteOn']:
+        if event_type in ['NoteOff', 'NoteOn']:
             midi_event.key = byte_queue.popleft()
             midi_event.velocity = byte_queue.popleft()
-
         # Otherwise just store it in a byte string.
-        if event_type_str in ['Program', 'ChannelKeyPressure']:
+        elif event_type in ['Program', 'ChannelKeyPressure']:
             midi_event.data = _pop_bytes(byte_queue, 1)
         else:
             midi_event.data = _pop_bytes(byte_queue, 2)
