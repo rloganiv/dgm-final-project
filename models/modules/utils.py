3bf272417647bc2168b6fe85b49dd73615ed634f
Alex Boyd <alexjb@uci.edu>
2019-05-23 14:09:50 -0700

    Initialized modules copied from HuggingFace's GPT2 code.

---
+++ models/modules/utils.py
@@ -0,0 +1,23 @@
+import math
+import torch
+from torch import nn
+
+def gelu(x):
+    return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+class Conv1D(nn.Module):
+    def __init__(self, nf, nx):
+        super(Conv1D, self).__init__()
+        self.nf = nf
+        w = torch.empty(nx, nf)
+        nn.init.normal_(w, std=0.02)
+        self.weight = Parameter(w)
+        self.bias = Parameter(torch.zeros(nf))
+
+    def forward(self, x):
+        size_out = x.size()[:-1] + (self.nf,)
+        x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
+        x = x.view(*size_out)
+        return x
+
+
