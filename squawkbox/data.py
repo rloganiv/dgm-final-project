d0e3665d96235b2827d61c088aef5a9e352700fc
rloganiv <rloganiv@gmail.com>
2019-05-23 15:44:50 -0700

    Added a collation function that pads and combines instance dicts

--- squawkbox/data.py
+++ squawkbox/data.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import torch
 from torch.utils.data import Dataset
 
@@ -7,6 +9,38 @@ NOTE_EVENTS = ['note:%i:%i' % (i, j) for i in range(128) for j in range(128)]
 WAIT_EVENTS = ['wait:%i' % i for i in range(1024)]  # Note: might need more waits
 IDX_TO_TOKEN = [*SPECIAL_TOKENS, *NOTE_EVENTS, *WAIT_EVENTS]
 TOKEN_TO_IDX = {token: i for i, token in enumerate(IDX_TO_TOKEN)}
+
+
+def pad_and_combine_instances(batch):
+    """
+    A collate function for padding and combining instance dictionaries.
+    """
+    batch_size = len(batch)
+    max_field_lengths = defaultdict(int)
+    for instance in batch:
+        for field, tensor in instance.items():
+            if len(tensor.size()) == 0:
+                continue
+            elif len(tensor.size()) == 1:
+                max_field_lengths[field] = max(max_field_lengths[field], tensor.size()[0])
+            elif len(tensor.size()) > 1:
+                raise ValueError('Padding multi-dimensional tensors not supported')
+
+    out_dict = {}
+    for i, instance in enumerate(batch):
+        for field, tensor in instance.items():
+            if field not in out_dict:
+                if field in max_field_lengths:
+                    size = (batch_size, max_field_lengths[field])
+                else:
+                    size = (batch_size,)
+                out_dict[field] = tensor.new_zeros(size)
+            if field in max_field_lengths:
+                out_dict[field][i, :tensor.size()[0]] = tensor
+            else:
+                out_dict[field][i] = tensor
+
+    return out_dict
 
 
 class MidiDataset(Dataset):
@@ -55,9 +89,9 @@ class MidiDataset(Dataset):
 
                 tokens = [TOKEN_TO_IDX[x] for x in line.split()]
                 instance = {
-                    'src': torch.LongTensor(tokens[:-1]),
-                    'tgt': torch.LongTensor(tokens[1:]),
-                    'timestamp': torch.FloatTensor(timestamps[:-1]),
+                    'src': torch.tensor(tokens[:-1], dtype=torch.int64),
+                    'tgt': torch.tensor(tokens[1:], dtype=torch.int64),
+                    'timestamp': torch.tensor(timestamps[:-1], dtype=torch.float32),
                 }
                 instances.append(instance)
 
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,6 @@
 import torch
 
-from squawkbox.data import MidiDataset, TOKEN_TO_IDX
-
+from squawkbox.data import TOKEN_TO_IDX, MidiDataset, pad_and_combine_instances
 
 
 def test_read_instance():
@@ -27,3 +26,28 @@ def test_read_instance():
     expected_timestamp = [0, 0, 1]
     expected_timestamp_tensor = torch.FloatTensor(expected_timestamp)
     assert torch.equal(instance['timestamp'], expected_timestamp_tensor)
+
+
+def test_pad_and_combine_instances():
+    batch = [
+        {
+            'a': torch.tensor([1,2,3], dtype=torch.int64),
+            'b': torch.tensor(1, dtype=torch.float32)
+        },
+        {
+            'a': torch.tensor([1,2], dtype=torch.int64),
+            'b': torch.tensor(2, dtype=torch.float32)
+        }
+    ]
+    out_dict = pad_and_combine_instances(batch)
+
+    # Check that output has the correct fields
+    assert set(out_dict.keys()) == {'a', 'b'}
+
+    # Check that sequences handled correctly
+    expected_a = torch.tensor([[1,2,3], [1,2,0]], dtype=torch.int64)
+    assert torch.equal(out_dict['a'], expected_a)
+
+    # Check that scalars handled correctly
+    expected_b = torch.tensor([1,2], dtype=torch.float32)
+    assert torch.equal(out_dict['b'], expected_b)
