2201b79905f45b660c14ed684f5980e4fc3e7c8f
Alex Boyd <alexjb@uci.edu>
2019-06-13 19:20:46 -0700

    Added option to condition on other midi files.

--- squawkbox/commands/sample.py
+++ squawkbox/commands/sample.py
@@ -10,13 +10,15 @@ from typing import Deque, IO, List, Tuple
 import time
 import yaml
 import torch
+from torch.utils.data import DataLoader
 import numpy as np
+import sys
 
 from squawkbox.midi import Midi
 from squawkbox.tokenizer import Tokenizer
 from squawkbox.models import Model
 from squawkbox.modules.sampler import Sampler
-
+from squawkbox.data import IDX_TO_TOKEN, MidiDataset, pad_and_combine_instances
 
 logger = logging.getLogger(__name__)
 
@@ -70,6 +72,26 @@ def _sample(args):
     model = model.to(dev)
 
     
+    if args.conditional is not None:
+        dataset = MidiDataset(
+            args.conditional,
+            transforms=None,
+            embedding_type=config["training"].get("embedding_type", "wallclock")
+        )
+        loader = DataLoader(
+            dataset,
+            batch_size=1,
+            shuffle=False,
+            num_workers=0 ,
+            collate_fn=pad_and_combine_instances
+        )
+        instance = next(iter(loader))
+        src = instance["src"][:,:args.conditional_len].expand(args.num_samples,-1)
+        timestamps = instance["timestamp"][:,:args.conditional_len].expand(args.num_samples,-1)
+    else:
+        src = None
+        timestamps = None
+
     with torch.no_grad():
         logger.info("Generating samples")
         sampler = Sampler(
@@ -82,8 +104,8 @@ def _sample(args):
         )
 
         samples = sampler(
-            src = None, 
-            timestamps = None,
+            src = src, 
+            timestamps = timestamps,
             batch_size = args.num_samples,
             dev = dev
         )
@@ -93,6 +115,13 @@ def _sample(args):
     for i, sample in enumerate(samples):
         tokens = " ".join(sample)
         with open(samples_folder / "sample_{}.midi".format(i) , 'wb') as midi_file:
+            midi_file.write(tokenizer.detokenize(tokens))
+    
+    if args.conditional is not None:
+        original_ids = instance["src"].squeeze().tolist()
+        original_toks = [IDX_TO_TOKEN[idx] for idx in original_ids]
+        tokens = " ".join(original_toks)
+        with open(samples_folder / "original.midi", 'wb') as midi_file:
             midi_file.write(tokenizer.detokenize(tokens))
 
     logger.info("Saving complete")
@@ -111,6 +140,8 @@ if __name__ == "__main__":
     parser.add_argument('--cuda-device', type=int, help='CUDA device num', default=None)
     parser.add_argument('--top-k', type=int, help='K for top-k based sampling scheme.', default=None)
     parser.add_argument('--top-p', type=float, help='P for top-p (nucleus) based sampling scheme.', default=None)
+    parser.add_argument('--conditional', type=Path, help="path to .txt file containing tokenized a midi file to condition samples on - will only condition on first midi sequence")
+    parser.add_argument('--conditional_len', type=int, help="number of tokens in conditional sequence to condition on", default=512)
     args, _ = parser.parse_known_args()
 
     if os.environ.get("DEBUG"):
