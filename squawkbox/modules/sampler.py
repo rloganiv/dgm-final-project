118b81756868f4fe9a5c0385af21cf93593be54b
Robert L. Logan IV <rloganiv@gmail.com>
2019-06-14 11:38:28 -0700

    Sampling corrections (#29)
    * Late night tweaks
    
    * Fixed sampleing for positional embeddings

--- squawkbox/modules/sampler.py
+++ squawkbox/modules/sampler.py
@@ -6,7 +6,7 @@ from squawkbox.data import TOKEN_TO_IDX, IDX_TO_TOKEN
 
 class Sampler(nn.Module):
 
-    def __init__(self, decoder, embedding_type, temp = None, top_k = None, top_p = None, max_length = 4096):
+    def __init__(self, decoder, embedding_type, temp=None, top_k=None, top_p=None, max_length=4096):
         super(Sampler, self).__init__()
 
         self.decoder = decoder
@@ -21,17 +21,11 @@ class Sampler(nn.Module):
         self.cont = TOKEN_TO_IDX["continue"]
         self.pos_adjustment = nn.Embedding(len(IDX_TO_TOKEN), 1)
 
-        if embedding_type == "wallclock":
-            self.pos_adjustment.weight.data = torch.FloatTensor(
-                [[float(tok.split(":")[1])] if tok.split(":")[0] == "wait" else [0.0] for tok in IDX_TO_TOKEN]            )
-        elif embedding_type == "positional":
-            self.pos_adjustment.weight.data = torch.FloatTensor(
-                [[1.0] for tok in IDX_TO_TOKEN]
-            )
-        else:
-            self.pos_adjustment.weight.data = torch.FloatTensor(
-                [[0.0] for tok in IDX_TO_TOKEN]
-            )
+    def _delta_time(self, sample, dev):
+        tokens = [IDX_TO_TOKEN[x] for x in sample.squeeze().tolist()]
+        waits = [int(x.split(':')[-1]) if 'wait' in x else 0 for x in tokens]
+        return torch.tensor(waits, device=dev,dtype=torch.float32).unsqueeze(-1)
+
 
     def _temper(self, logits):
         """
@@ -43,9 +37,6 @@ class Sampler(nn.Module):
             t = self.temp
 
         return F.softmax(logits / t, dim = -1)
-
-        
-
 
     def _sample(self, probs):
         """
@@ -61,25 +52,39 @@ class Sampler(nn.Module):
         Sample based on probs with shape (batch_size, vocab_size) with a top k scheme
         Returns sample of type LongTensor and shape (batch_size, 1).
         """
-        sorted_probs, sorted_indices = probs.sort(dim = -1)
-        masked_probs = torch.zeros_like(probs).scatter(1, sorted_indices[:, :self.top_k], probs)
-        return self._sample(masked_probs) 
+        sorted_probs, sorted_indices = probs.sort(dim=-1, descending=True)
+        new_distribution = sorted_probs[:, :self.top_k] / sorted_probs.sum(-1, keepdim=True)
+        sample_indices = self._sample(new_distribution)
+        sample = sorted_indices.gather(-1, sample_indices)
+        return sample
+
 
     def _sample_top_p(self, probs):
         """
         Sample based on probs with shape (batch_size, vocab_size) with a top p scheme
         Returns sample of type LongTensor and shape (batch_size, 1).
         """
-        sorted_probs, sorted_indices = probs.sort(dim = -1)
-        cum_probs = probs.cumsum(dim = -1)
+        sorted_probs, sorted_indices = probs.sort(dim=-1)
+        cum_probs = probs.cumsum(dim=-1)
         sorted_mask = (sorted_probs <= self.top_p).type(torch.float32)
         masked_probs = probs * torch.zeros_like(probs).scatter(1, sorted_indices, sorted_mask)
         return self._sample(masked_probs)
 
-    def forward(self, src = None, timestamps = None, batch_size = None, dev = None, **kwargs):
+    def _to_tokens(self, src):
+        out = []
+        for sequence in src:
+            tokens = [IDX_TO_TOKEN[i] for i in sequence.tolist()]
+            if 'end' in tokens:
+                cutoff = tokens.index('end')
+                out.append(tokens[:cutoff])
+            else:
+                out.append(tokens)
+        return out
+
+    def forward(self, src=None, timestamps=None, batch_size=None, dev=None, **kwargs):
         """
-        Generate samples either conditioned on 'src' or from nothing. Either both 'src' and 'timestamps' 
-        must be specified or 'batch_size'. If all three are specified, the 'batch_size' will be 
+        Generate samples either conditioned on 'src' or from nothing. Either both 'src' and 'timestamps'
+        must be specified or 'batch_size'. If all three are specified, the 'batch_size' will be
         overridden by the size of the first dimenion of the two tensors.
 
         Input:
@@ -87,32 +92,28 @@ class Sampler(nn.Module):
         - 'timestamps': A FloatTensor of shape (batch_size, sequence_length)
         - 'batch_size': An int specifying how many samples to generate.
         Output:
-        - A list of samples, each sample being a list of str tokens. 
+        - A list of samples, each sample being a list of str tokens.
         """
 
         if src is not None:
             assert(timestamps is not None)
-            assert((len(src.shape) == len(timestamps.shape)) and 
-                   (src.shape[0] == timestamps.shape[0]) and 
+            assert((len(src.shape) == len(timestamps.shape)) and
+                   (src.shape[0] == timestamps.shape[0]) and
                    (src.shape[1] == timestamps.shape[1]))
             batch_size = src.shape[0]
         else:
             assert(batch_size is not None)
             src = torch.LongTensor([[self.SOS]]).expand(batch_size, 1).to(dev)
             timestamps = torch.zeros(batch_size, 1, dtype=torch.float32).to(dev)
-        
-        samples = [src]
-        sample = src
+
         hidden = None
-        flags = sample[:,-1] == self.EOS
         for _ in range(self.max_length):
-            output = self.decoder(src = sample, timestamps = timestamps, tgt = None, hidden = hidden)
-            # only need to keep track of the last timestamps (only matters when conditioning on src)
-            if timestamps.shape[1] > 1:
-                timestamps = timestamps[:, -1].unsqueeze(-1)
-            # get the last step's logits for the next step and temper them 
+
+            with torch.no_grad():
+                output = self.decoder(src, timestamps=timestamps)
+
+            # get the last step's logits for the next step and temper them
             probs = self._temper(output["logits"][:, -1, :])
-            hidden = output["hidden"]
 
             if self.top_k is not None:
                 sample = self._sample_top_k(probs)
@@ -121,22 +122,16 @@ class Sampler(nn.Module):
             else:
                 sample = self._sample(probs)
 
-            samples.append(sample)
-            timestamps += self.pos_adjustment(sample.squeeze())
-            
-            flags |= sample[:, -1] == self.EOS
-            if flags.sum().item() == batch_size:
-                break
+            src = torch.cat((src, sample), -1)
 
+            if self.embedding_type == 'positional':
+                new_timestamp = timestamps[:, -1].unsqueeze(-1) + 1
+            elif self.embedding_type == 'wallclock':
+                delta = self._delta_time(sample, dev)
+                new_timestamp = timestamps[:, -1].unsqueeze(-1) + delta
+            timestamps = torch.cat((timestamps, new_timestamp), -1)
 
-        # append EOS token in case sequence didn't finish before max_length
-        samples.append(torch.LongTensor([[self.EOS]]).expand(batch_size, 1))
-        samples = torch.cat(samples, dim = -1).tolist()
-        # remove tokens past EOS
-        samples = [sample[:(sample.index(self.EOS)+1)] for sample in samples]
-        # convert ids to tokens
-        samples = [[IDX_TO_TOKEN[idx] for idx in sample] for sample in samples]
+        samples = self._to_tokens(src)
+
         return samples
-
-        
 
